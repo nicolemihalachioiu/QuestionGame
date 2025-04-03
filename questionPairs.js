14fed735174894ee7d04f4bfd613b3ced4cf3ae9
nicolemihalachioiu <nicole.mihalachioiu@gmail.com>
2025-04-03 15:10:03 -0400

    edited the question pairs

--- questionPairs.js
+++ questionPairs.js
@@ -1,31 +1,106 @@
 module.exports = [
     {
-      player: "What's your favorite fruit?",
-      imposter: "What's your favorite vegetable?"
+        player: "On a scale from 1-100 how much do your parents love you?",
+        imposter: "Pick a number from 1-100"
     },
     {
-      player: "What's your dream vacation spot?",
-      imposter: "Where would you never go on vacation?"
+        player: "What country has the most attractive people?",
+        imposter: "What country has the best food in the world?"
     },
     {
-      player: "What's your favorite number?",
-      imposter: "What's a random number you dislike?"
+        player: "If you could be any height what height would you be?",
+        imposter: "If you could choose your s/o's height what would it be?"
     },
     {
-      player: "What's your favorite movie genre?",
-      imposter: "What's a movie genre you avoid?"
+        player: "What animal would you like to turn into?",
+        imposter: "What animal is the tastiest?"
     },
     {
-      player: "What's your favorite season?",
-      imposter: "What's your least favorite season?"
+        player: "What tv show would you show to your kids",
+        imposter: "What is the show you watched the most recently?"
     },
     {
-      player: "What's your go-to drink?",
-      imposter: "What's a drink you never order?"
+        player: "Hours of work you do per week?",
+        imposter: "Pick a number from 1-110"
     },
     {
-      player: "What time of day do you like most?",
-      imposter: "What time of day do you like least?"
+        player: "How long do you stay in bed after you wake up?",
+        imposter: "Give a time between 1 minute and 4 hours"
+    },
+    {
+        player: "What celeb do you want to be stuck on a deserted island with?",
+        imposter: "Who is your least favourite celeb?"
+    },
+    {
+        player: "What is the most romantic thing you can say to someone?",
+        imposter: "What is your fave movie quote?"
+    },
+    {
+        player: "What country would you most like to visit?",
+        imposter: "What is the most dangerous country?"
+    },
+    {
+        player: "What is the best item to have during a zombie apocalypse?",
+        imposter: "What is the worst gift to receive?"
+    },
+    {
+        player: "What is the best time of day to go to the gym?",
+        imposter: "Best time to wake up on a chill weekend?"
+    },
+    {
+        player: "What is your go to karaoke song?",
+        imposter: "Song that makes you cry?"
+    },
+    {
+        player: "Who in the group do you secretly look up to?",
+        imposter: "Who is most likely to go to jail in the group?"
+    },
+    {
+        player: "What country would you most like to visit?",
+        imposter: "What is the most dangerous country?"
+    },
+    {
+        player: "What is a good age to have your first kid?",
+        imposter: "At what age did you have your first kiss?"
+    },
+    {
+        player: "What is your favourite superhero?",
+        imposter: "Dream career?"
+    },
+    {
+        player: "Where would you go if you could teleport anywhere?",
+        imposter: "Your least favourite restaurant?"
+    },
+    {
+        player: "What fictional world would you most want to live in?",
+        imposter: "What is the scariest movie?"
+    },
+    {
+        player: "What is your best physical feature?",
+        imposter: "What is the grossest body part?"
+    },
+    {
+        player: "What is your favourite musical artist?",
+        imposter: "Name the most overrated musical artist?"
+    },
+    {
+        player: "If you won the lottery what percent would you give to your parents?",
+        imposter: "How much do you tip at a restaurant?"
+    },
+    {
+        player: "What is the most amount of times you have masterbated in a day?",
+        imposter: "Pick a number from 1-30"
+    },
+    {
+        player: "Craziest place you've ever taken a shit?",
+        imposter: "Best place to relax?"
+    },
+    {
+        player: "Number of times you poop in a day?",
+        imposter: "Pick a number from 0-10"
+    },
+    {
+        player: "Pick a the best ship in the group",
+        imposter: "What two people in the group hang out the least?"
     }
   ];
-  
