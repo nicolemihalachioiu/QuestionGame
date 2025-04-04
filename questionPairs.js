c87a1ac6ab86625060b5ee216a49178f86ba4cdb
nicolemihalachioiu <nicole.mihalachioiu@gmail.com>
2025-04-04 01:12:50 -0400

    Changed name and added more questions(35)

--- questionPairs.js
+++ questionPairs.js
@@ -1,102 +1,142 @@
 module.exports = [
-    {
+    { //1
         player: "On a scale from 1-100 how much do your parents love you?",
         imposter: "Pick a number from 1-100"
     },
-    {
+    { //2
         player: "What country has the most attractive people?",
         imposter: "What country has the best food in the world?"
     },
-    {
+    { //3
         player: "If you could be any height what height would you be?",
         imposter: "If you could choose your s/o's height what would it be?"
     },
-    {
+    { //4
         player: "What animal would you like to turn into?",
         imposter: "What animal is the tastiest?"
     },
-    {
+    { //5
         player: "What tv show would you show to your kids",
         imposter: "What is the show you watched the most recently?"
     },
-    {
+    { //6
         player: "Hours of work you do per week?",
         imposter: "Pick a number from 1-110"
     },
-    {
+    { //7
         player: "How long do you stay in bed after you wake up?",
         imposter: "Give a time between 1 minute and 4 hours"
     },
-    {
+    { //8
         player: "What celeb do you want to be stuck on a deserted island with?",
         imposter: "Who is your least favourite celeb?"
     },
-    {
+    { //9
         player: "What is the most romantic thing you can say to someone?",
         imposter: "What is your fave movie quote?"
     },
-    {
+    { //10
         player: "What country would you most like to visit?",
         imposter: "What is the most dangerous country?"
     },
-    {
+    { //11
         player: "What is the best item to have during a zombie apocalypse?",
         imposter: "What is the worst gift to receive?"
     },
-    {
+    { //12
         player: "What is the best time of day to go to the gym?",
         imposter: "Best time to wake up on a chill weekend?"
     },
-    {
+    { //13
         player: "What is your go to karaoke song?",
         imposter: "Song that makes you cry?"
     },
-    {
+    { //14
         player: "Who in the group do you secretly look up to?",
         imposter: "Who is most likely to go to jail in the group?"
     },
-    {
+    { //15
         player: "What is a good age to have your first kid?",
         imposter: "At what age did you have your first kiss?"
     },
-    {
+    { //16
         player: "What is your favourite superhero?",
         imposter: "Dream career?"
     },
-    {
+    { //17
         player: "Where would you go if you could teleport anywhere?",
         imposter: "Your least favourite restaurant?"
     },
-    {
+    { //18
         player: "What fictional world would you most want to live in?",
         imposter: "What is the scariest movie?"
     },
-    {
+    { //19
         player: "What is your best physical feature?",
         imposter: "What is the grossest body part?"
     },
-    {
+    { //20
         player: "What is your favourite musical artist?",
         imposter: "Name the most overrated musical artist?"
     },
-    {
+    { //21
         player: "If you won the lottery what percent would you give to your parents?",
         imposter: "How much do you tip at a restaurant?"
     },
-    {
+    { //22
         player: "What is the most amount of times you have masterbated in a day?",
         imposter: "Pick a number from 1-30"
     },
-    {
+    { //23
         player: "Craziest place you've ever taken a shit?",
         imposter: "Best place to relax?"
     },
-    {
+    { //24
         player: "Number of times you poop in a day?",
         imposter: "Pick a number from 0-10"
     },
-    {
+    { //25
         player: "Pick a the best ship in the group",
         imposter: "What two people in the group hang out the least?"
+    },
+    { //26
+        player: "What's your dream job if money didn’t matter?",
+        imposter: "What's the most boring job you can think of?"
+    },
+    { //27
+        player: "What is your favourite RG outfit?",
+        imposter: "What would you wear on a night out?"
+    },
+    { //28
+        player: "What's the most expensive thing you've ever bought?",
+        imposter: "What's something you refuse to spend money on?"
+    },
+    { //29
+        player: "What emoji do you use the most?",
+        imposter: "What emoji do you hate?"
+    },
+    { //30
+        player: "What's your biggest turn-on?",
+        imposter: "What's something that turns you off immediately?"
+    },
+    { //31
+        player: "What’s your most toxic trait in relationships?",
+        imposter: "What’s your biggest dating green flag?"
+    },
+    { //32
+        player: "Who's your celebrity crush",
+        imposter: "Who's someone you'd never sleep with, even for a million dollars?"
+    },
+    { //33
+        player: "Who in the group would survive the longest in a horror movie?",
+        imposter: "Who in the group would die first in a horror movie?"
+    },
+    { //34
+        player: "Who's the chillest person in the group?",
+        imposter: "Who's the most dramatic person in the group?"
+    },
+    { //35
+        player: "Who in the group would win a reality show?",
+        imposter: "Who would get voted off the island first?"
     }
   ];
